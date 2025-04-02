8f0a9046458e95581a8d27f9c980e4f892e24b23
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2025-04-02 16:57:50 +0500

    Added models in admin site

--- eateries/admin.py
+++ eateries/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'created_at', 'updated_at')
+    search_fields = ('name', 'code')
+    list_filter = ('created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(models.WiFi)
+class WiFiAdmin(admin.ModelAdmin):
+    list_display = ('name', 'password', 'created_at', 'updated_at')
+    search_fields = ('name', 'password')
+    list_filter = ('created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(models.Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'created_at', 'updated_at')
+    search_fields = ('name', 'phone_number')
+    list_filter = ('created_at', 'updated_at', 'currency')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    list_filter = ('created_at', 'updated_at')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'category',
+        'created_at',
+        'updated_at'
+    )
+    search_fields = (
+        'name',
+        'price',
+        'organization__name',
+        'organization__short_name'
+    )
+    list_filter = ('created_at', 'updated_at', 'category', 'organization')
+    date_hierarchy = 'created_at'
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
