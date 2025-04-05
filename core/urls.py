c680da3a89e86b8ae6ea7d6e2db219ef1a30ece6
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2025-04-05 23:25:11 +0500

    Added path for settings

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path, include
@@ -34,3 +36,5 @@ urlpatterns = [
         cache_timeout=0), name='schema-json'),
     path("api/v1/", include("api.urls")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
